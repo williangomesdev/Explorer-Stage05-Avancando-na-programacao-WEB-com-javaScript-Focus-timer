50c2e2b46b532aed3cf12bb6969dac64b556dec2
Willian Amaro Gomes <willianamaroti@gmail.com>
2022-04-25 20:48:16 -0300

    Principios do clean code e programação declarativa

--- js/script.js
+++ js/script.js
@@ -8,27 +8,36 @@ const minutesDisplay = document.querySelector(".minutes");
 const secondsDisplay = document.querySelector(".seconds");
 let minutes;
 
+function resetControls() {
+  playButton.classList.remove("hide");
+  pauseButton.classList.add("hide");
+  setButton.classList.remove("hide");
+  stopButton.classList.add("hide");
+}
+
+function updateTimerDisplay(minutes, seconds) {
+  minutesDisplay.textContent = String(minutes).padStart(2, "0");
+  secondsDisplay.textContent = String(seconds).padStart(2, "0");
+}
+
 function countDown() {
   setTimeout(function () {
     let seconds = Number(secondsDisplay.textContent);
     let minutes = Number(minutesDisplay.textContent);
-    
+
+    updateTimerDisplay(minutes, 0);
+
+    if (minutes <= 0) {
+      resetControls();
+      return;
+    }
 
     if (seconds <= 0) {
       seconds = 2;
-      minutesDisplay.textContent = String(minutes - 1).padStart(2, "0");
+      --minutes;
     }
 
-    secondsDisplay.textContent = String(seconds - 1).padStart(2, "0");
-
-    if (minutes <= 0) {
-      playButton.classList.remove("hide");
-      pauseButton.classList.add("hide");
-      setButton.classList.remove("hide");
-      stopButton.classList.add("hide");
-
-      return;
-    }
+    updateTimerDisplay(minutes, String(seconds - 1));
 
     countDown();
   }, 1000);
@@ -51,14 +60,12 @@ pauseButton.addEventListener("click", function () {
 
 setButton.addEventListener("click", function () {
   minutes = prompt("Quantos minutos?");
-  minutesDisplay.textContent = String(minutes).padStart(2, "0");
+  updateTimerDisplay(minutes, 0);
 });
 
+
 stopButton.addEventListener("click", function () {
-  playButton.classList.remove("hide");
-  pauseButton.classList.add("hide");
-  setButton.classList.remove("hide");
-  stopButton.classList.add("hide");
+  resetControls();
 });
 
 soundOnButton.addEventListener("click", function () {
