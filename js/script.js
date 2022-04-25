ec4fb9e8f028aba0abae84135c585d7c9a3e16fe
Willian Amaro Gomes <willianamaroti@gmail.com>
2022-04-25 17:24:03 -0300

    Refatorando para as novas funcionalidades

--- js/script.js
+++ js/script.js
@@ -2,18 +2,36 @@ const playButton = document.querySelector(".play");
 const pauseButton = document.querySelector(".pause");
 const stopButton = document.querySelector(".stop");
 const setButton = document.querySelector(".set");
+const soundOnButton = document.querySelector(".soundOn");
+const soundOffButton = document.querySelector(".soundOff");
 
-//Event-driven = programação orientada a eventos
+//Eventos
 playButton.addEventListener("click", function () {
   playButton.classList.add("hide");
   pauseButton.classList.remove("hide");
+  setButton.classList.add("hide");
+  stopButton.classList.remove("hide");
 });
 
 pauseButton.addEventListener("click", function () {
-  pauseButton.classList.add("hide");
   playButton.classList.remove("hide");
+  pauseButton.classList.add("hide");
+  setButton.classList.add("hide");
 });
 
-//Programação imperativa = dar ordens passo a passo para ser feito
-//Programação declarativa = somente o que tem que fazer
-//Callback = toda função que é mandada como argumento, após uma ação  chamo uma função de volta
+stopButton.addEventListener("click", function () {
+  playButton.classList.remove("hide");
+  pauseButton.classList.add("hide");
+  setButton.classList.remove("hide");
+  stopButton.classList.add("hide");
+});
+
+soundOnButton.addEventListener("click", function () {
+  soundOnButton.classList.add("hide");
+  soundOffButton.classList.remove("hide");
+});
+
+soundOffButton.addEventListener("click", function () {
+  soundOnButton.classList.remove("hide");
+  soundOffButton.classList.add("hide");
+});
