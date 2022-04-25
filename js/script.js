4087aa9f2100e5c2459e631db7faa00197afd143
Willian Amaro Gomes <willianamaroti@gmail.com>
2022-04-25 20:22:25 -0300

    Avançando na lógica com pequenos passos

--- js/script.js
+++ js/script.js
@@ -4,6 +4,35 @@ const stopButton = document.querySelector(".stop");
 const setButton = document.querySelector(".set");
 const soundOnButton = document.querySelector(".soundOn");
 const soundOffButton = document.querySelector(".soundOff");
+const minutesDisplay = document.querySelector(".minutes");
+const secondsDisplay = document.querySelector(".seconds");
+let minutes;
+
+function countDown() {
+  setTimeout(function () {
+    let seconds = Number(secondsDisplay.textContent);
+    let minutes = Number(minutesDisplay.textContent);
+    
+
+    if (seconds <= 0) {
+      seconds = 2;
+      minutesDisplay.textContent = String(minutes - 1).padStart(2, "0");
+    }
+
+    secondsDisplay.textContent = String(seconds - 1).padStart(2, "0");
+
+    if (minutes <= 0) {
+      playButton.classList.remove("hide");
+      pauseButton.classList.add("hide");
+      setButton.classList.remove("hide");
+      stopButton.classList.add("hide");
+
+      return;
+    }
+
+    countDown();
+  }, 1000);
+}
 
 //Eventos
 playButton.addEventListener("click", function () {
@@ -11,12 +40,18 @@ playButton.addEventListener("click", function () {
   pauseButton.classList.remove("hide");
   setButton.classList.add("hide");
   stopButton.classList.remove("hide");
+  countDown();
 });
 
 pauseButton.addEventListener("click", function () {
   playButton.classList.remove("hide");
   pauseButton.classList.add("hide");
   setButton.classList.add("hide");
+});
+
+setButton.addEventListener("click", function () {
+  minutes = prompt("Quantos minutos?");
+  minutesDisplay.textContent = String(minutes).padStart(2, "0");
 });
 
 stopButton.addEventListener("click", function () {
